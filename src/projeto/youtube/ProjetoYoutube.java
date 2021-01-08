27396d616c3c7855fe8da167974bc6bb159b1775
Robson <robsonshockwavegithub@gmail.com>
2021-01-08 13:04:55 -0300

    Classe Visualização (agregação), três métodos avaliar criados (polimorfismo de sobrecarga)

--- src/projeto/youtube/ProjetoYoutube.java
+++ src/projeto/youtube/ProjetoYoutube.java
@@ -13,8 +13,23 @@ public class ProjetoYoutube {
             g[0] = new Gafanhoto("Jubileu", 22, "M", "juba");
             g[1] = new Gafanhoto("Creuza", 29, "F", "creuzita");
             
+            Visualizacao visu[] = new Visualizacao[5];
+            visu[0] = new Visualizacao(g[0], v[2]); // Jubileu assiste HTML5
+            visu[0].avaliar();
+            System.out.println(visu[0].toString());
+            
+            visu[1] = new Visualizacao(g[0], v[1]); // Jubileu assiste JS
+            visu[0].avaliar(87.0f);
+            System.out.println(visu[0].toString());
+            /*
+            System.out.println("VÍDEOS\n-----------------------------");
             System.out.println(v[0].toString());
+            System.out.println(v[1].toString());
+            System.out.println(v[2].toString());
+            System.out.println("\nGAFANHOTOS\n-----------------------------");
             System.out.println(g[0].toString());
+            System.out.println(g[1].toString());
+            */
     }
     
 }
